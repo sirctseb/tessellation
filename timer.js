225510df8f0fe4976505e8aee75b949e39a3bf62
Christopher Best <bestchris@gmail.com>
2012-01-25 11:52:36 -0500

    implemented methods of timer

--- timer.js
+++ timer.js
@@ -5,7 +5,12 @@ var timer = (function() {
 
 	// methods
 	return {
+		// put a first time in the timer
 		startTimer: function(name) {
+			this.lap(name);
+		},
+		// add the current time to the timer history and return the lap time
+		lap: function(name) {
 			// default name is '' so we don't interfere with actual names
 			if(name === undefined) {
 				name = '';
@@ -17,7 +22,64 @@ var timer = (function() {
 			}
 
 			// add the current time to the timer
-			timers[name].push(new Date());
+			timers[name].push(Date.now());
+
+			// return the difference between now and the previous time
+			// make sure there is a previous time, if not, return now
+			if(timers[name].length < 2) {
+				return timers[name][0];
+			} else {
+				return timers[name][timers[name].length-1] - timers[name][timers[name].length-2];
+			}
+		},
+		// return the time at wich lap occured
+		getTime: function(name, lap) {
+			// check that the timer exists
+			if(!timers[name]) {
+				return undefined;
+			}
+
+			// if no lap supplied, return time of the most recent lap
+			if(lap === undefined) {
+				lap = -1;
+			}
+
+			// compute positive index from negative index
+			if(lap < 0) {
+				lap = timers[name].length + lap;
+
+				// if lap was too negative, return undefined
+				if(lap < 0) {
+					return undefined;
+				}
+			}
+
+			// return the time
+			return timers[name][lap];
+		},
+		// return the time a given lap took
+		getLapTime: function(name, lap) {
+			// check that the timer exists
+			if(!timers[name]) {
+				return undefined;
+			}
+
+			// if no lap supplied, return lap time of the most recent lap
+			if(lap === undefined) {
+				lap = -1;
+			}
+
+			// get the time of lap and previous lap
+			var lapTime = this.getTime(name, lap);
+			var prevLapTime = this.getTime(name, lap - 1);
+
+			// if there was a problem getting either time, return
+			if(lapTime === undefined || prevLapTime === undefined) {
+				return undefined;
+			}
+
+			// return the difference in times
+			return lapTime - prevLapTime;
 		}
 	}
 }());
