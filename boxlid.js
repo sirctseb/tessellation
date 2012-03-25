ff593c01c0497b08ddd9b4d7426cdbe36177fe6e
Christopher Best <bestchris@gmail.com>
2012-03-25 16:13:05 -0400

    made handle sizes pixels instead of percents made borders on panels and offsetting margins implemented dragging on top panel

--- boxlid.js
+++ boxlid.js
@@ -4,6 +4,7 @@
 	$.fn.boxlid = function(method) {
 		var methods = {
 			init: function(options) {
+				var that = this;
 				// initialize box lid layout
 
 				// wrapper init
@@ -36,6 +37,58 @@
 				$(".boxlid-bottom-panel", this).prepend(
 					$("<div></div>", {'class': 'boxlid-handle boxlid-hor-handle boxlid-bottom-handle'})
 				);
+
+				// set sizes in pixels
+				// set width in pixels for top and left
+				$(".boxlid-top-panel, .boxlid-left-panel", this).width(function(index, width) {
+					return width;
+				});
+				// set width in pixels for right and bottom
+				$(".boxlid-right-panel", this).width(this.width() - $(".boxlid-top-panel").width());
+				$(".boxlid-bottom-panel", this).width(this.width() - $(".boxlid-left-panel").width());
+
+				// set height in pixels for top and right
+				$(".boxlid-top-panel, .boxlid-left-panel", this).height(function(index, height) {
+					return height;
+				});
+				// set height in pixels for left and bottom
+				$(".boxlid-left-panel", this).height(this.height() - $(".boxlid-top-panel").height());
+				$(".boxlid-bottom-panel", this).height(this.height() - $(".boxlid-right-panel").height());
+
+				// TODO show handle image in handle on mouse over
+				//log.log('putting mouse move on', 'handle');
+				/*$(".boxlid-top-handle", this).on("mousemove.boxlid", 
+				})
+				// disable until mousedow
+				.off("mousemove.boxlid");*/
+				//log.log('took mouse move off', 'handle');
+
+				// add interaction handlers
+				$(".boxlid-top-handle", that).mousedown(function(event) {
+					$("body").on("mousemove.boxlid", function(event) {
+						// TODO dragging stuff
+
+						// get top panel
+						toppanel = $(".boxlid-top-panel", $(this).parent().parent());
+
+						// set new height of top panel
+						//log.log(event.pageY - $(this).offset().top, 'handle');
+						toppanel.height(event.pageY - toppanel.offset().top);
+
+						// set new height of left panel
+						$(".boxlid-left-panel", toppanel.parent()).height(toppanel.parent().height() - toppanel.height());
+
+						log.log('mouse moved', 'handle');
+					});
+					log.log('putting mouse move on, because down', 'handle');
+				});
+
+				// up handlers
+				$("body").mouseup(function(event) {
+					//$(".boxlid-top-handle", that).off("mousemove.boxlid");
+					$("body").off("mousemove.boxlid");
+					log.log('taking mouse move off, because up', 'handle');
+				});
 			},
 			// blah blah blah
 		};
