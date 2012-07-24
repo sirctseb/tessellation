dd347f67a66ce55a11954195180e974e597c9981
Christopher Best <bestchris@gmail.com>
2012-07-24 11:42:08 -0400

    selection stuff

--- FadeMenu.dart
+++ FadeMenu.dart
@@ -17,6 +17,7 @@ class FadeMenuElement {
 
 class FadeMenuSection extends FadeMenuElement{
   Element _header;
+  bool _selectable = false;
   FadeMenuSection(options) : super(options) {
     // add classes from options
     if(options.classes) {
@@ -36,6 +37,42 @@ class FadeMenuSection extends FadeMenuElement{
 
   Element get header()            => _header.text;
           set header(String text) => _header.text = text;
+
+  bool  get selected()          =>  root.classes.contains("selected");
+        set selected(bool sel)  =>  (sel && _selectable)
+                                      ? root.classes.add("selected")
+                                      : root.classes.remove("selected");
+  // TODO a better interface might be public properties for callbacks,
+  // and getter/setter for selectable
+
+  // selection callbacks
+  var _onSelect = null;
+  var _onDeselect = null;
+  // TODO unregister click handlers for selectable == false
+  // make the section selectable or non-selectable
+  setSelectable(bool selectable, [callbacks]) {
+    // set callbacks if supplied
+    _onSelect = callbacks["onSelect"];
+    _onDeselect = callbacks["onDeselect"];
+    if(selectable) {
+      // register click handlers
+      // TODO should callbacks be called if selection is changed via accessors?
+      _header.on.click.add((event) {
+        // if currently selected
+        if(this.selected) {
+          // deselect
+          this.selected = false;
+          // call callback
+          if(_onSelect != null) _onSelect();
+        } else {
+          // otherwise, select
+          this.selected = true;
+          // call callback
+          if(_onDeselect != null) _onDeselect();
+        }
+      });
+    }
+  }
 }
 
 class FadeMenuCollapsableSection extends FadeMenuSection {
