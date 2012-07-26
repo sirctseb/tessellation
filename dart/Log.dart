a28116278d74fd56dfeb0576647508d30684bc02
Christopher Best <bestchris@gmail.com>
2012-07-26 13:45:32 -0400

    added named logs

--- dart/Log.dart
+++ dart/Log.dart
@@ -8,14 +8,33 @@ class Log {
   int level;
   // the set of currently active groups
   Set<String> groups;
+  // the name of this log
+  String _name;
+  String get name => _name;
 
-  Log([int level = 0, Collection<String> groups = null]) {
+  // the existing named logs
+  static Map<String, Log> logs = {};
+
+  factory Log([int level = 0, Collection<String> groups = null]) {
+    return new Log.named("default", level, groups);
+  }
+
+  // create named log
+  Log._named(this._name, [int level = 0, Collection<String> groups = null]) {
     this.level = level;
     if(groups != null) {
       this.groups = new Set.from(groups);
     } else {
       this.groups = new Set();
     }
+    // add to map
+    logs[_name] = this;
+  }
+  factory Log.named(String name) {
+    if(logs.containsKey(name)) {
+      return logs[name];
+    }
+    return new Log(name: name);
   }
 
   bool log(content, [var qualifier]) {
