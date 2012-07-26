264e46d40b7c1b2926d66c098109a86e190d5aca
Christopher Best <bestchris@gmail.com>
2012-07-26 13:46:30 -0400

    more boxlid implementation
    implemented panel size accessors
    added other drag handlers
    private convenience accessors to panel elements

--- dart/BoxLid.dart
+++ dart/BoxLid.dart
@@ -5,16 +5,73 @@
 #import("dart:html");
 
 class BoxLid {
+  // the root element of the boxlid layout
   Element root;
+ 
+  // convenience accessors to panels
+  Element get _leftPanel => root.query(".boxlid-left-panel");
+  Element get _rightPanel => root.query(".boxlid-right-panel");
+  Element get _topPanel => root.query(".boxlid-top-panel");
+  Element get _bottomPanel => root.query(".boxlid-bottom-panel");
+  Element get _centerPanel => root.query(".boxlid-center");
+
+  // the width or height of each panel
   int _left, _right, _top, _bottom;
-  int get left => return _left;
+  // the last width or height of each panel before it was closed
+  int _openLeft, _openRight, _openTop, _openBottom;
+  // the width or height of the panels when closed;
+  int _closedSize;
+
+  // accessors for the widths and heights
+  // setters automatically update dependent sizes
+  int get left => _left;
   int set left (value) {
     _left = value;
-    // TODO set other sizes
+    // set sizes of elements
+    _leftPanel.style.width = "${value}px";
+    _bottomPanel.style.width = "${root.style.height.parseInt() - value}px";
+    _setCenterSize();
+  }
+  int get top => _top;
+  int set top(value) {
+    _top = value;
+    // set sizes of elements
+    root.query(".boxlid-top-panel").style.height = "${value}px";
+    root.query(".boxlid-left-panel").style.height = "${root.style.height.parseInt() - value}px";
+    _setCenterSize();
+  }
+  int get bottom => _botttom;
+  int set bottom(value) {
+    _bottom = value;
+    // set sizes of elements
+    _bottomPanel.style.height = "${value}px";
+    _rightPanel.style.height = "${root.style.height.parseInt() - value}px";
+    _setCenterSize();
+  }
+  int get right => _right;
+  int set right(value) {
+    _right = value;
+    // set sizes of elements
+    _rightPanel.style.width = "${value}px";
+    _topPanel.style.width = "${root.style.width.parseInt() - value}px";
+    _setCenterSize();
+  }
+
+  /** Set the size of the center panel from the sizes of the side panels **/
+  _setCenterSize() {
+    int width = root.style.width.parseInt() -
+                  root.query(".boxlid-left-panel").style.width.parseInt() -
+                  _rightPanel.style.width.parseInt();
+    int height = root.style.height.parseInt() -
+                  _topPanel.style.height.parseInt() -
+                  _bottomPanel.style.height.parseInt();
+    _centerPanel.style.width = "${width}px";
+    _centerPanel.style.height = "${height}px";
   }
   // TODO setters
 
   BoxLid(Element element) {
+    _closedSize = 10;
     root = element;
     // add wrapper class
     root.classes.add("boxlid-wrapper");
@@ -48,7 +105,25 @@ class BoxLid {
 
     // add interaction handlers
     topHandle.on.drag.add((event) {
-      this.top = event.pageY - this.topPanel.offset().top;
+      this.top = event.pageY - this._topPanel.offset().top;
     });
+
+    leftHandle.on.drag.add((event) {
+      this.left = event.pageX - this._leftPanel.offset().left;
+    });
+
+    rightHandle.on.drag.add((event) {
+      this.right = _rightPanel.offset().left + _rightPanel.width - event.pageX;
+    });
+
+    bottomHandle.on.drag.add((event) {
+      this.bottom = _bottomPanel.offset().top + _bottomPanel.height - event.pageY;
+    });
+
+    // click handles to open / close them
+    leftHandle.on.click.add((event) {
+      //if(_leftPanel.)
+    })
+
   }
 }
