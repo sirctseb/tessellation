a321d1d512c328d9715bdee864c67bfcbcab4c9b
Christopher Best <bestchris@gmail.com>
2012-08-01 14:45:32 -0400

    removed old template code

--- dart/TessellationApp.dart
+++ dart/TessellationApp.dart
@@ -3,18 +3,7 @@
 #import('FadeMenu.dart');
 #import('BoxLid.dart');
 
-num rotatePos = 0;
-
 void main() {
   queryAll(".fade-menu").forEach((menu) => new FadeMenu(menu));
   new BoxLid(query(".boxlid-wrapper"));
-}
-
-void rotateText(Event event) {
-  rotatePos += 360;
-
-  var textElement = query("#text");
-
-  textElement.style.transition = "1s";
-  textElement.style.transform = "rotate(${rotatePos}deg)";
 }
