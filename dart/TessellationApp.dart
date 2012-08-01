5606623e854e29fb22384ec97da72830219f5dfa
Christopher Best <bestchris@gmail.com>
2012-08-01 15:09:20 -0400

    started app class to be root controller

--- dart/TessellationApp.dart
+++ dart/TessellationApp.dart
@@ -1,9 +1,24 @@
-
 #import('dart:html');
 #import('FadeMenu.dart');
 #import('BoxLid.dart');
 
 void main() {
-  queryAll(".fade-menu").forEach((menu) => new FadeMenu(menu));
-  new BoxLid(query(".boxlid-wrapper"));
+  new TessellationWebApp();
+}
+
+class TessellationWebApp {
+
+	BoxLid _layout;
+	FadeMenu _pathStyleMenu, _gridSettingMenu, _pointMenu, _keyPathMenu, _scaleMenu, _parameterizationMenu;
+
+	TessellationWebApp() {
+		// initialize web app
+		_layout = new BoxLid(query(".boxlid-wrapper"));
+		_pathStyleMenu = new FadeMenu(query("#pathstylecontainer"));
+		_gridSettingMenu = new FadeMenu(query("#gridsettingcontainer"));
+		_pointMenu = new FadeMenu(query("#pointcontainer"));
+		_keyPathMenu = new FadeMenu(query("#keypathcontainer"));
+		_scaleMenu = new FadeMenu(query("#scalecontainer"));
+		_parameterizationMenu = new FadeMenu(query("#parameterizationcontainer"));
+	}
 }
