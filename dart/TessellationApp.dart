d1e95d014b27f8435e0a19834aa212e403a0d604
Christopher Best <bestchris@gmail.com>
2012-08-01 15:31:59 -0400

    added main tessellation to app

--- dart/TessellationApp.dart
+++ dart/TessellationApp.dart
@@ -8,17 +8,26 @@ void main() {
 
 class TessellationWebApp {
 
+	// the boxlid layout class
 	BoxLid _layout;
+
+	// instances of fade menus
 	FadeMenu _pathStyleMenu, _gridSettingMenu, _pointMenu, _keyPathMenu, _scaleMenu, _parameterizationMenu;
+
+	// the Tessellation in the primary Tessellation view
+	Tessellation _mainTessellation;
 
 	TessellationWebApp() {
 		// initialize web app
 		_layout = new BoxLid(query(".boxlid-wrapper"));
+
 		_pathStyleMenu = new FadeMenu(query("#pathstylecontainer"));
 		_gridSettingMenu = new FadeMenu(query("#gridsettingcontainer"));
 		_pointMenu = new FadeMenu(query("#pointcontainer"));
 		_keyPathMenu = new FadeMenu(query("#keypathcontainer"));
 		_scaleMenu = new FadeMenu(query("#scalecontainer"));
 		_parameterizationMenu = new FadeMenu(query("#parameterizationcontainer"));
+
+		_mainTessellation = new Tessellation();
 	}
 }
