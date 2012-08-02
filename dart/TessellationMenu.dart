f07120f80d68753252ce73a9aa09b8ae818bb47f
Christopher Best <bestchris@gmail.com>
2012-08-02 16:13:03 -0400

    started lattice and vector fade menu views

--- dart/TessellationMenu.dart
+++ dart/TessellationMenu.dart
@@ -64,6 +64,81 @@ class TessellationMenu {
   }
 }
 
+/** [FadeMenuLatticeSection] is a custom [FadeMenuCollapsableSection] for
+  * viewing information about a [Lattice] of a [Tessellation]
+  */
+class FadeMenuLatticeSection {
+
+  FadeMenuLatticeSection(Lattice lattice) {
+    _lattice = lattice;
+
+    // create sub elements
+    _v1display = this.addElement();
+    _v2display = this.addElement();
+
+    // initialize sub elements
+    _v1display.root.classes.addAll(["v1display", "vdisplay"]);
+    _v2display.root.classes.addAll(["v2display", "vdisplay"]);
+  }
+
+  Lattice _lattice;
+  Lattice get => _lattice;
+  Lattice set lattice (Lattice value) {
+    _lattice = value;
+
+    // TODO update view
+  }
+}
+
+class FadeMenuVectorElement {
+
+  FadeMenuVectorElement(Vector2 vector, controller) {
+    _vector = vector;
+
+    // initialize views
+
+    // add default views
+
+    // create edit button
+    Element editButton = new Element.tag("div");
+    editButton.classes.addAll(["editButton", "vdefault"]);
+    editButton.text = "edit";
+    // add click handler
+    editButton.on.click.add((event) { controller.editVector(vector); });
+    root.nodes.add(editButton);
+
+    // create text display
+    Element textDisplay = new Element.tag("div");
+    textDisplay.classes.addAll(["latticeVec", "vdefault"]);
+    // TODO pretty print version
+    textDisplay.text = vector.toString();
+    // add click handler
+    textDisplay.on.click.add((event) { controller.beginEditLattice(vector); });
+    root.nodes.add(textDisplay);
+
+
+    // add editing views
+
+    // create submit button
+    Element submitButton = new Element.tag("div");
+    submitButton.classes.addAll(["vecEditSubmit", "vedit"]);
+    submitButton.text = "submit";
+    submitButton.on.click.add((event) { controller.finishEditVector(vector); });
+
+    // create first label
+    Element xLabel = new Element.tag("div");
+    xlabel
+  }
+
+  Vector2 _vector;
+  Vector2 get vector => _vector;
+  Vector2 set vector(Vector2 value) {
+    _vector = value;
+
+    // TODO update view
+  }
+}
+
 /** [FadeMenuShapeSection] is a custom [FadeMenuCollapsableSection] for
   * displaying information about polygons in a [Tessellation]
   */
